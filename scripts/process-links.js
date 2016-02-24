616d810ca3d9bd83a719143b4d64e46e348e83d0
Jacob Carpenter <jacobcarpenter@gmail.com>
2016-02-23 23:24:37 -0800

    Fix more whitespace.

--- scripts/process-links.js
+++ scripts/process-links.js
@@ -15,19 +15,18 @@ fs.readFile(path.join(__dirname, '../data/users.json'), (err, data) => {
 
 		const links = yaml.safeLoad(data);
 
-		const distinctLinks = Object.keys(links)
-			.reduce(
-				(acc, source) => Object.assign({},
-					acc,
-					{ [source]: links[source].filter(target => !acc[target] || acc[target].indexOf(source) === -1) }),
-				{});
+		const distinctLinks = Object.keys(links).reduce(
+			(acc, source) => Object.assign({},
+				acc,
+				{ [source]: links[source].filter(target => !acc[target] || acc[target].indexOf(source) === -1) }),
+			{});
 
 		const linkOutput = Object.keys(distinctLinks)
-            .map(source => distinctLinks[source].map(target => ({
-                source: userIds[source],
-                target: userIds[target],
-            })))
-            .reduce((acc, curr) => [...acc, ...curr], []);
+			.map(source => distinctLinks[source].map(target => ({
+				source: userIds[source],
+				target: userIds[target],
+			})))
+			.reduce((acc, curr) => [...acc, ...curr], []);
 
 		const output = Object.assign({}, users, { links: linkOutput });
 		fs.writeFile(path.join(__dirname,'../public/merged.json'), JSON.stringify(output, null, 2), err => {
